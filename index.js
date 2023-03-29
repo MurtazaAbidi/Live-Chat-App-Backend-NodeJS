b2b79931d838cede99680aac5a18c415dbf0535e
Murtaza Abidi <murtaza2476175@gmail.com>
2023-03-29 06:04:59 +0500

    Chat App Backend

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+const http = require('http');
+const cors = require('cors');
+const { Server } = require("socket.io");
+
+app.use(cors());
+require('dotenv').config();
+
+const server = http.createServer(app);
+app.get('', (req, res)=>{
+    console.log("helloworld")
+    res.send('hello')
+})
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+
+    socket.on("join_room", (data)=>{
+        socket.join (data);
+        console.log(`User with ID: ${socket.id} joined room: ${data}`)
+    })
+
+    socket.on("send_message", (data)=>{
+        console.log(data);
+        socket.to(data.room).emit("receive_message", data);
+    })
+
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", socket.id);
+    })
+});
+
+
+
+
+server.listen(process.env.PORT, () => {
+    console.log("SERVER running at port "+`${process.env.PORT}`)
+})
